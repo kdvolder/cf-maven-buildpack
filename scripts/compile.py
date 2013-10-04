d535bca165e2119c1f287653fed86f12a5df0b1a
Daniel Mikusa <dmikusa@gopivotal.com>
2013-10-04 12:20:34 -0400

    First stab at a compile script

--- scripts/compile.py
+++ scripts/compile.py
@@ -15,52 +15,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from build_pack_utils import CloudFoundryUtil
-from build_pack_utils import CloudFoundryInstaller
+def log_run_error(cmd, retcode, stdout, stderr):
+    print 'Comand [%s] failed with [%d]' % (' '.join(cmd), retcode)
+    print 'STDOUT:'
+    print stdout
+    print 'STDERR:'
+    print stderr
+    raise BuilderExit()
+
 
 if __name__ == '__main__':
-    # Do this first.  Sets up STDOUT and gives us access to basic info
-    #  like build directory, cache directory, memory limit, temp dir,
-    #  and build pack directory.  Also has utilities for loading JSON
-    #  configuration files.
-    cf = CloudFoundryUtil()
-
-    # Load default and user config, merge the two
-    default_cfg = cf.load_json_config_file_from(cf.BP_DIR, 'defaults.json')
-    user_cfg = cf.load_json_config_file_from(cf.BUILD_DIR, 'config.json')
-    if user_cfg:
-        print 'Custom configuration found, overriding default options'
-        cfg = dict(default_cfg.items() + user_config.items())
-    else:
-        cfg = default_cfg
-   
-    # Create our installer.  This is a utility that helps us download
-    #  install required packages.  In addition, it performs basic caching
-    #  and attempts to be smart about downloading a file or using the
-    #  cached version.
-    installer = CloudFoundryInstaller(cf, cfg)
-
-    # Download & Install Java.  By default, installed to the build pack
-    #  directory, but can be customized by setting '_PACKAGE_INSTALL_DIR'
-    #  key in the configuration.
-    java_install_dir = installer.install_binary('JAVA')
-
-    # Download & Install Maven.  Same as Java.
-    maven_install_dir = installer.install_binary('MAVEN')
-
-    # Run Maven to compile and build
-    #  Run default command 'mvn package' or custom command
-    if 'MAVEN_BUILD_COMMAND' in cfg.keys():
-        parts = cfg['MAVEN_BUILD_COMMAND'].split(' ')
-        command = parts[0]
-        args = parts[1:]
-    else:
-        command = 'mvn'
-        args = ('package',)
-    CloudFoundryRunner.run_from_directory(maven_install_dir, 
-                                          command, args)
-
-    # Create Run Script
-    #  Default -> Look at command in manifest
-    #  Config -> MVN_RUN_COMMAND
+    Builder()
+        .configure()
+            .default_config()
+            .user_config()
+            .done()
+        .install()
+            .package('JAVA')
+            .package('MAVEN')
+            .done()
+        .run()
+            .command(cfg.get('MAVEN_BUILD_COMMAND', './bin/mvn package'))
+            .out_of('MAVEN_INSTALL_PATH')
+            .with_shell()
+            .on_fail(log_run_err)
+            .done()
+        .create_start_script()
+            .environment_variable()
+                .export()
+                .name('JAVA_HOME')
+                .value('JAVA_INSTALL_PATH')
+            .environment_variable()
+                .export()
+                .name('M2_HOME')
+                .value('MAVEN_INSTALL_PATH')
+            .command()
+                .run('mvn')
+                .with_argument('tomcat:run')
+            .write()
 
